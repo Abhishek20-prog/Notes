d2ecefa11a1b340b26e7a8d60d4175388dc1af9c
Abhishek20-prog <Abhishekkumastudent1234@gmail.com>
2026-07-09 18:21:16 +0530

    initial commit

--- NOTE/src/App.jsx
+++ NOTE/src/App.jsx
@@ -1,10 +1,10 @@
-
+import Form from './Components/Form'
 import Sidebar from './Components/Sidebar'
 const App = () => {
   return (
     <div className="h-screen bg-gray-800 text-amber-50 flex border-2 border-gray-500 items-center rounded-3xl"> 
       <Sidebar />
-        
+        <Form/>
       
 
     </div>
--- NOTE/src/Components/Sidebar.jsx
+++ NOTE/src/Components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import plus from "../assets/plus-svgrepo-com.svg"
 
+
 const Sidebar = () => {
+  
+
   return (
     <div className="h-[95%] w-30 border-2 border-gray-500 flex flex-col gap-8 items-center p-3 bg-gray-400 mx-4 rounded-3xl">
       <h1 className="text-2xl font-bold">Notes</h1>
