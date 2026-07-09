cc7a55f08d7122e6cd03bea458a50b4f774bff17
Abhishek20-prog <Abhishekkumastudent1234@gmail.com>
2026-07-09 18:27:07 +0530

    initial commit

--- NOTE/src/App.jsx
+++ NOTE/src/App.jsx
@@ -1,5 +1,6 @@
 import Form from './Components/Form'
 import Sidebar from './Components/Sidebar'
+import { useState } from 'react'
 const App = () => {
   return (
     <div className="h-screen bg-gray-800 text-amber-50 flex border-2 border-gray-500 items-center rounded-3xl"> 
--- NOTE/src/Components/Form.jsx
+++ NOTE/src/Components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const Form = () => {
-    const [showForm, setShowForm] = useState(true)
+   
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const handleSubmit = (e) => {
@@ -11,15 +11,15 @@ const Form = () => {
         setContent('')
     }
   return (
+  
     <div className=" h-[93%] w-[93%] flex flex-col gap-8 items-center justify-center p-3 mx-4 rounded-3xl">
-    {showForm=== true &&(<div className=" h-[93%] w-[93%] flex flex-col gap-8 items-center justify-center p-3 mx-4 rounded-3xl">
         <form className="flex flex-col gap-4 p-4 mx-auto justify-center items-center border-gray-500 border-2 rounded-3xl bg-gray-600" onSubmit={handleSubmit}>
             <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
             <textarea placeholder="Content" value={content} onChange={(e) => setContent(e.target.value)} />
             <button type="submit" className="bg-blue-500 text-white p-2 rounded-3xl">Add Note</button>
         </form>
         
-    </div>)}</div>
+    </div>
   )
 }
 
