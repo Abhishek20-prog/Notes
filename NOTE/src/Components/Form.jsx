faa59f89b49a0f7cb2635051f2e2e9fbdee4e84b
Abhishek20-prog <Abhishekkumastudent1234@gmail.com>
2026-07-09 18:38:28 +0530

    initial commit

--- NOTE/src/Components/Form.jsx
+++ NOTE/src/Components/Form.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react'
+
 
 const Form = () => {
    
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(title, content)
-        setTitle('')
-        setContent('')
-    }
+    
   return (
   
     <div className=" h-[93%] w-[93%] flex flex-col gap-8 items-center justify-center p-3 mx-4 rounded-3xl">
-        <form className="flex flex-col gap-4 p-4 mx-auto justify-center items-center border-gray-500 border-2 rounded-3xl bg-gray-600" onSubmit={handleSubmit}>
-            <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <textarea placeholder="Content" value={content} onChange={(e) => setContent(e.target.value)} />
+        <form className="flex flex-col gap-4 p-4 mx-auto justify-center items-center border-gray-500 border-2 rounded-3xl bg-gray-600" >
+            <input type="text" placeholder="Title"  />
+            <textarea placeholder="Content"  />
             <button type="submit" className="bg-blue-500 text-white p-2 rounded-3xl">Add Note</button>
         </form>
         
